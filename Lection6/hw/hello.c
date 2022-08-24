8cdca5b4196e1dfd5ecc005caf7206ab0b0d1c0f
Sergii Piatakov <sergii.piatakov@globallogic.com>
2022-08-24 12:21:26 +0300

    task 6: add a procfs interface to existed module
    Add a procfs interface to the module to provide information about sysfs
    access. The module has three files in the one common folder:
        /proc/
        └── hello_iface       a common interface directory;
            ├── read_num      return a number of readings from sysfs;
            ├── write_num     return a number of writings in sysfs;
            └── module_owner  return a module owner.
    All files have only read permissions and don't support writing
    operations.
    
    Usage example:
     - initial state:
     # cat /proc/hello_iface/read_num
     # 0
     # cat /proc/hello_iface/write_num
     # 0
     # cat /proc/hello_iface/module_owner
     # module owner
    
     - interact with the module via sysfs:
     # echo one > /sys/kernel/hello/list
     # echo two > /sys/kernel/hello/list
     # cat /sys/kernel/hello/list
     # one
     # two
    
     - check statistics:
     # cat /proc/hello_iface/read_num
     # 1
     # cat /proc/hello_iface/write_num
     # 2
    
    The patch has been successfully tested by:
    $ checkpatch.pl --no-tree -f hello.c
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- Lection6/hw/hello.c
+++ Lection6/hw/hello.c
@@ -5,6 +5,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/kobject.h>
+#include <linux/path.h>
+#include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/sysfs.h>
 #include <linux/types.h>
@@ -20,6 +22,15 @@ static struct list_head list;
 
 
 
+struct sysfs_ops_stat {
+	unsigned int reads;
+	unsigned int writes;
+};
+
+static struct sysfs_ops_stat stats = {0, 0};
+
+
+
 static ssize_t on_sysfs_show(struct kobject *object, struct kobj_attribute *attribute, char *buffer)
 {
 	struct list_entry *entry = NULL;
@@ -31,6 +42,8 @@ static ssize_t on_sysfs_show(struct kobject *object, struct kobj_attribute *attr
 		sprintf(buffer + count, "%s", entry->payload);
 		count += strlen(entry->payload);
 	}
+
+	stats.reads++;
 
 	return count;
 }
@@ -45,6 +58,8 @@ static ssize_t on_sysfs_store(struct kobject *object, struct kobj_attribute *att
 
 	list_add_tail(&entry->list, &list);
 
+	stats.writes++;
+
 	return count;
 }
 
@@ -57,23 +72,170 @@ static struct kobject *hello_kobject;
 
 
 
+#define PROC_DIR_NAME			"hello_iface"
+#define PROC_FILE_READS_NAME	"read_num"
+#define PROC_FILE_WRITES_NAME	"write_num"
+#define PROC_FILE_OWNER_NAME	"module_owner"
+#define PROC_MESSAGE_CAPACITY	16
+
+// Helper structure to keep together all information about message transfer related
+// to the specific file.
+//
+// There are the following conventions:
+//  - `was_send` is true means, that data transfer was completed during the previous
+//    call;
+//  - `strlen(message)` is zero means that we are in the first call of a new session
+//    and the new message hasn't been created yet;
+//  - `create` each file in procfs must have its own function for message creation.
+//
+// At different transfer stages the structure members will have the following values.
+//
+// Initial ------> Construct -----> Transfering ----> Complete -----> Initial
+// -------         ---------        -----------       --------       ---------
+// was_send=false  was_send=false   was_send=false    was_send=true  was_send=false
+// len(message)=0  len(message)!=0  len(message)!=0   memssage=?     len(message)=0
+// pos=0           pos=0            pos<len(message)  pos=?          pos=0
+struct proc_message_transfer {
+	char message[PROC_MESSAGE_CAPACITY];
+	bool was_send;
+	loff_t pos;
+	void (*create)(char *buffer, size_t capacity);
+};
+
+// Constants to map procfs files with their transfer contexts.
+enum proc_message_transfer_id {
+	PROC_READS_ID	= 0,
+	PROC_WRITES_ID	= 1,
+	PROC_OWNER_ID	= 2,
+};
+
+void proc_message_create_reads(char *buffer, size_t capacity)
+{
+	snprintf(buffer, capacity, "%u\n", stats.reads);
+}
+
+void proc_message_create_writes(char *buffer, size_t capacity)
+{
+	snprintf(buffer, capacity, "%u\n", stats.writes);
+}
+
+void proc_message_create_owner(char *buffer, size_t capacity)
+{
+	snprintf(buffer, capacity, "module owner\n");
+}
+
+static struct proc_message_transfer proc_transfers[] = {
+	{ {}, false, 0, proc_message_create_reads},
+	{ {}, false, 0, proc_message_create_writes},
+	{ {}, false, 0, proc_message_create_owner}
+};
+
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_file_reads;
+static struct proc_dir_entry *proc_file_writes;
+static struct proc_dir_entry *proc_file_owner;
+
+static ssize_t on_proc_read(struct file *file, char __user *buffer, size_t count, loff_t *pos)
+{
+	int message_size;
+	int remains;
+	struct proc_message_transfer *transfer = NULL;
+
+	if (!strcmp(file->f_path.dentry->d_name.name, PROC_FILE_READS_NAME))
+		transfer = &proc_transfers[PROC_READS_ID];
+	else if (!strcmp(file->f_path.dentry->d_name.name, PROC_FILE_WRITES_NAME))
+		transfer = &proc_transfers[PROC_WRITES_ID];
+	else if (!strcmp(file->f_path.dentry->d_name.name, PROC_FILE_OWNER_NAME))
+		transfer = &proc_transfers[PROC_OWNER_ID];
+	else
+		transfer = NULL;
+
+	if (!transfer) {
+		pr_warn("Hello module. Unexpected file entry!\n");
+		return 0;
+	}
+
+	if (transfer->was_send)
+		goto stop_loop;
+
+	if (!strlen(transfer->message))
+		transfer->create(transfer->message, PROC_MESSAGE_CAPACITY);
+
+	transfer->pos += *pos;
+	if (transfer->pos < 0)
+		transfer->pos = 0;
+
+	message_size = strlen(transfer->message) - transfer->pos;
+	if (message_size <= 0)
+		goto stop_loop;
+
+	message_size = min_t(size_t, count, message_size);
+	remains = copy_to_user(buffer, &transfer->message[transfer->pos], message_size);
+	transfer->pos += message_size - remains;
+	transfer->was_send = transfer->pos >= strlen(transfer->message)	// Normal conditions.
+						|| message_size == remains;					// Error conditions.
+
+	return message_size - remains;
+
+stop_loop:
+	transfer->was_send = false;
+	transfer->pos = 0;
+	transfer->message[0] = '\0';
+
+	return 0;
+}
+
+static struct proc_ops on_proc_ops = {
+	.proc_read = on_proc_read,
+};
+
+
+
 int hello_init(void)
 {
-	int result = 0;
-
 	INIT_LIST_HEAD(&list);
 
 	hello_kobject = kobject_create_and_add("hello", kernel_kobj);
 	if (!hello_kobject)
-		return -ENOMEM;
+		goto error_list;
 
-	result = sysfs_create_file(hello_kobject, &list_attribute.attr);
-	if (result)
-		kobject_put(hello_kobject);
+	if (sysfs_create_file(hello_kobject, &list_attribute.attr))
+		goto error_kobject;
 
-	pr_info("Hello! Init.\n");
+	proc_dir = proc_mkdir(PROC_DIR_NAME, NULL);
+	if (!proc_dir)
+		goto error_sysfs;
 
-	return result;
+	proc_file_reads = proc_create(PROC_FILE_READS_NAME, 0444, proc_dir, &on_proc_ops);
+	if (!proc_file_reads)
+		goto error_proc_dir;
+
+	proc_file_writes = proc_create(PROC_FILE_WRITES_NAME, 0444, proc_dir, &on_proc_ops);
+	if (!proc_file_writes)
+		goto error_proc_file;
+
+	proc_file_owner = proc_create(PROC_FILE_OWNER_NAME, 0444, proc_dir, &on_proc_ops);
+	if (!proc_file_owner)
+		goto error_proc_files;
+
+	pr_info("Hello module is inited.\n");
+
+	return 0;
+
+error_proc_files:
+	remove_proc_entry(PROC_FILE_WRITES_NAME, proc_dir);
+error_proc_file:
+	remove_proc_entry(PROC_FILE_READS_NAME, proc_dir);
+error_proc_dir:
+	remove_proc_entry(PROC_DIR_NAME, NULL);
+error_sysfs:
+	kobject_put(hello_kobject);
+error_kobject:
+	kobject_del(hello_kobject);
+error_list:
+	list_del(&list);
+
+	return -ENOMEM;
 }
 
 void hello_exit(void)
@@ -90,9 +252,11 @@ void hello_exit(void)
 	}
 	list_del(&list);
 
+	proc_remove(proc_dir);
+
 	kobject_del(hello_kobject);
 
-	pr_info("Hello! Cleanup.\n");
+	pr_info("Hello module is exited.\n");
 }
 
 module_init(hello_init);
--- Lection6/hw/test.sh
+++ Lection6/hw/test.sh
@@ -4,12 +4,20 @@ dmesg -c
 lsmod
 insmod hello.ko
 
+cat /proc/hello_iface/read_num
+cat /proc/hello_iface/write_num
+cat /proc/hello_iface/module_owner
+
 echo 'Hello' > /sys/kernel/hello/list
 echo 'from' > /sys/kernel/hello/list
 echo 'me' > /sys/kernel/hello/list
 echo '!' > /sys/kernel/hello/list
 
 cat /sys/kernel/hello/list
+
+cat /proc/hello_iface/read_num
+cat /proc/hello_iface/write_num
+cat /proc/hello_iface/module_owner
 
 rmmod hello
 lsmod
